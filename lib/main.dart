9f0c1bd2c41493b75758b6b6dd0a5104f479dacb
Stevenson Chittumuri <mchittum123@gmail.com>
2022-04-08 19:15:00 -0400

    signup page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lets_connect/signup_page.dart';
 import 'package:lottie/lottie.dart';
 
 void main() {
@@ -28,21 +29,11 @@ MaterialColor createMaterialColor(Color color) {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Let's Connect",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: createMaterialColor(Color.fromARGB(255, 255, 170, 12)),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -52,16 +43,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -145,7 +126,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: MediaQuery.of(context).size.width / 5.5,
                 child: ElevatedButton(
                   onPressed: () {
-                    // Respond to button press
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>SignupPage()));
                   },
                   child: Text(
                     'Login',
